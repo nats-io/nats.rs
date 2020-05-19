8bc62d5517695f0173394a5ea8ee5dd8bbbd8037
Tyler Neely <t@jujit.su>
2020-05-19 14:13:54 +0200

    Name all threads created for normal system operation

--- src/shared_state.rs
+++ src/shared_state.rs
@@ -199,10 +199,16 @@ impl SharedState {
             shared_state: shared_state.clone(),
         };
 
-        let inbound_thread = thread::spawn(move || inbound.read_loop());
+        let inbound_thread = thread::Builder::new()
+            .name("nats_inbound_read_loop".to_string())
+            .spawn(move || inbound.read_loop())
+            .expect("threads should be spawnable");
 
         let outbound_state = shared_state.clone();
-        let outbound_thread = thread::spawn(move || outbound_state.outbound.flush_loop());
+        let outbound_thread = thread::Builder::new()
+            .name("nats_outbound_flush_loop".to_string())
+            .spawn(move || outbound_state.outbound.flush_loop())
+            .expect("threads should be spawnable");
 
         {
             inject_delay();
