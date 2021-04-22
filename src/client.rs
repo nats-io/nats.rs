4543da5bab220abbc28027aa99f80a9b82d0901b
Tyler Neely <t@jujit.su>
2021-04-22 12:09:56 +0200

    Remove result return type from infallible close method.

--- src/client.rs
+++ src/client.rs
@@ -219,7 +219,7 @@ impl Client {
     }
 
     /// Closes the client.
-    pub(crate) fn close(&self) -> io::Result<()> {
+    pub(crate) fn close(&self) {
         // Inject random delays when testing.
         inject_delay();
 
@@ -250,8 +250,6 @@ impl Client {
             state.pongs.clear();
             drop(state);
         }
-
-        Ok(())
     }
 
     /// Kicks off the shutdown process, but doesn't wait for its completion.
