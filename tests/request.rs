bf672e507bcc572272319450ff993e89f1780de1
Jarema <melgaer@gmail.com>
2021-10-27 07:29:57 +0200

    Use smol in request test instead of async-std

--- tests/request.rs
+++ tests/request.rs
@@ -19,24 +19,27 @@ fn no_responders() -> Result<(), String> {
     }
 }
 
-#[async_std::test]
-async fn no_responders_async() -> Result<(), String> {
-    let nc = nats::asynk::Options::new().connect("demo.nats.io").await.unwrap();
-    let anc = nc.clone();
-    let (tx, rx) = mpsc::channel();
-    let child = task::spawn(async move   {
-        let sub = anc.subscribe("noresponders.>").await.unwrap();
-        tx.send(true).unwrap();
-        let msg =  sub.next().await.unwrap();
-        anc.publish(msg.reply.unwrap().as_str(), "pong").await.unwrap();
-    });
-    rx.recv().unwrap();
-    let req = nc.request("noresponders.asdf", "ping").await.unwrap();
-    match req.err {
-        Some(MessageError::NoResponders) => Err(String::from("found no responders error, should be ok")),
-        None => Ok(()),
-    }?;
-    assert_eq!("pong", from_utf8(&req.data).unwrap());
-    let _ = child.await;
-    Ok(())
+#[test]
+fn no_responders_async() -> Result<(), String> {
+    smol::block_on(async {
+        let nc = nats::asynk::Options::new().connect("demo.nats.io").await.unwrap();
+        let anc = nc.clone();
+        let (tx, rx) = mpsc::channel();
+        let child = task::spawn(async move   {
+            let sub = anc.subscribe("noresponders.>").await.unwrap();
+            tx.send(true).unwrap();
+            let msg =  sub.next().await.unwrap();
+            anc.publish(msg.reply.unwrap().as_str(), "pong").await.unwrap();
+        });
+        rx.recv().unwrap();
+        let req = nc.request("noresponders.asdf", "ping").await.unwrap();
+        match req.err {
+            Some(MessageError::NoResponders) => Err(String::from("found no responders error, should be ok")),
+            None => Ok(()),
+        }?;
+        assert_eq!("pong", from_utf8(&req.data).unwrap());
+        let _ = child.await;
+        Ok(())
+    })
+
 }
