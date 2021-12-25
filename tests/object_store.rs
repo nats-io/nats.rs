48653119d9a03022870e41f443c463983732cbc8
Jarema <melgaer@gmail.com>
2021-12-25 20:54:29 +0100

    Gate kv and object store behind unstable feature

--- tests/object_store.rs
+++ tests/object_store.rs
@@ -11,8 +11,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#![cfg(feature = "unstable")]
 use rand::prelude::*;
-use std::io::Read;
 
 mod util;
 
