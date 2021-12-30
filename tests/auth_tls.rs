a8c3fd4a3b53fdacd60dbce43a0df192fb8ee09f
Jarema <melgaer@gmail.com>
2021-12-30 13:46:40 +0100

    Use rustls-pemfile instead of custom parser for TLS

--- tests/auth_tls.rs
+++ tests/auth_tls.rs
@@ -20,11 +20,12 @@ fn basic_tls() -> io::Result<()> {
         )
         .connect(&s.client_url())?;
 
+    // test scenario where rootCA, client certificate and client key are all in one .pem file
     nats::Options::with_user_pass("derek", "porkchop")
-        .add_root_certificate(path.join("tests/configs/certs/rootCA.pem"))
+        .add_root_certificate(path.join("tests/configs/certs/client-all.pem"))
         .client_cert(
-            path.join("tests/configs/certs/client-cert.pem"),
-            path.join("tests/configs/certs/client-key.pem"),
+            path.join("tests/configs/certs/client-all.pem"),
+            path.join("tests/configs/certs/client-all.pem"),
         )
         .connect(&s.client_url())?;
 
