5425118e945df65c44097853d050c78a204ebf1f
Jarema <melgaer@gmail.com>
2022-04-04 23:11:19 +0200

    Add flush

--- nats-experimental/examples/pub.rs
+++ nats-experimental/examples/pub.rs
@@ -10,6 +10,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let now = Instant::now();
     let subject = String::from("foo");
     let dat = Bytes::from("bar");
+    client.flush().await?;
     for _ in 0..10_000_000 {
         client.publish(subject.clone(), dat.clone()).await?;
     }
