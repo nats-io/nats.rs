e96350118b5e52680d24355ccb05f2e96ec70012
Casper Beyer <caspervonb@pm.me>
2022-04-07 16:47:50 +0800

    Make test case a bit more idiomatic

--- nats-experimental/tests/client_tests.rs
+++ nats-experimental/tests/client_tests.rs
@@ -19,20 +19,21 @@ mod client {
 
     #[tokio::test]
     async fn basic_pub_sub() {
-        let s = nats_server::run_basic_server();
-        let mut con = nats_experimental::connect(s.client_url()).await.unwrap();
+        let server = nats_server::run_basic_server();
+        let mut client = nats_experimental::connect(server.client_url())
+            .await
+            .unwrap();
 
-        let mut sub = con.subscribe("foo".into()).await.unwrap();
+        let mut subscriber = client.subscribe("foo".into()).await.unwrap();
 
         for _ in 0..10 {
-            con.publish("foo".to_string(), bytes::Bytes::from("data"))
-                .await
-                .unwrap();
+            client.publish("foo".into(), "data".into()).await.unwrap();
         }
-        con.flush().await.unwrap();
+
+        client.flush().await.unwrap();
 
         let mut i = 0;
-        while tokio::time::timeout(tokio::time::Duration::from_millis(100), sub.next())
+        while tokio::time::timeout(tokio::time::Duration::from_millis(100), subscriber.next())
             .await
             .unwrap()
             .is_some()
