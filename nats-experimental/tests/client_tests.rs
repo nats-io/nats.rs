7a63304c6a4fabc4ae3ba61b23d7bbee4a878ee5
Casper Beyer <caspervonb@pm.me>
2022-04-07 16:41:14 +0800

    Scope client tests to a client module

--- nats-experimental/tests/client_tests.rs
+++ nats-experimental/tests/client_tests.rs
@@ -12,32 +12,36 @@
 // limitations under the License.
 
 mod nats_server;
-use futures_util::StreamExt;
 
-#[tokio::test]
-async fn basic_pub_sub() {
-    let s = nats_server::run_basic_server();
-    let mut con = nats_experimental::connect(s.client_url()).await.unwrap();
+mod client {
+    use super::nats_server;
+    use futures_util::StreamExt;
 
-    let mut sub = con.subscribe("foo".into()).await.unwrap();
+    #[tokio::test]
+    async fn basic_pub_sub() {
+        let s = nats_server::run_basic_server();
+        let mut con = nats_experimental::connect(s.client_url()).await.unwrap();
 
-    for _ in 0..10 {
-        con.publish("foo".to_string(), bytes::Bytes::from("data"))
-            .await
-            .unwrap();
-    }
-    con.flush().await.unwrap();
+        let mut sub = con.subscribe("foo".into()).await.unwrap();
 
-    let mut i = 0;
-    while tokio::time::timeout(tokio::time::Duration::from_millis(100), sub.next())
-        .await
-        .unwrap()
-        .is_some()
-    {
-        i += 1;
-        if i >= 10 {
-            break;
+        for _ in 0..10 {
+            con.publish("foo".to_string(), bytes::Bytes::from("data"))
+                .await
+                .unwrap();
         }
+        con.flush().await.unwrap();
+
+        let mut i = 0;
+        while tokio::time::timeout(tokio::time::Duration::from_millis(100), sub.next())
+            .await
+            .unwrap()
+            .is_some()
+        {
+            i += 1;
+            if i >= 10 {
+                break;
+            }
+        }
+        assert_eq!(i, 10);
     }
-    assert_eq!(i, 10);
 }
