137d41f8b970b59e73208bb141ac814d29adae43
Jarema <melgaer@gmail.com>
2022-04-11 20:48:00 +0200

    Move options to its own file

---
+++ nats-experimental/tests/tls.rs
@@ -0,0 +1,52 @@
+// Copyright 2020-2022 The NATS Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::io;
+use std::path::PathBuf;
+
+mod util;
+pub use util::*;
+
+#[tokio::test]
+async fn basic_tls() -> io::Result<()> {
+    let s = util::run_server("tests/configs/tls.conf");
+
+    assert!(nats_experimental::connect("nats://127.0.0.1")
+        .await
+        .is_err());
+
+    let path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+
+    nats_experimental::options::Options::new()
+        .add_root_certificates(path.join("tests/configs/certs/rootCA.pem"))
+        .add_client_certificates(
+            path.join("tests/configs/certs/client-cert.pem"),
+            path.join("tests/configs/certs/client-key.pem"),
+        )
+        .require_tls(true)
+        .connect(&s.client_url())
+        .await?;
+
+    // test scenario where rootCA, client certificate and client key are all in one .pem file
+    nats_experimental::options::Options::new()
+        .add_root_certificates(path.join("tests/configs/certs/client-all.pem"))
+        .add_client_certificates(
+            path.join("tests/configs/certs/client-all.pem"),
+            path.join("tests/configs/certs/client-all.pem"),
+        )
+        .require_tls(true)
+        .connect(&s.client_url())
+        .await?;
+
+    Ok(())
+}
