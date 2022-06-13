114bd2e6439f7d601dcc1df6293966fc3aa87d1a
Tomasz Pietrek <tomasz@nats.io>
2022-06-13 12:41:33 +0200

    Add docs for jetstream::stream module
    Signed-off-by: Tomasz Pietrek <tomasz@nats.io>

--- async-nats/src/jetstream/context.rs
+++ async-nats/src/jetstream/context.rs
@@ -52,7 +52,23 @@ impl Context {
         }
     }
 
-    /// Publish a message
+    /// Publish a message to a given subject associated with a stream and returns an acknowledgment from
+    /// the server that the message has been successfully delivered.
+    ///
+    /// If the stream does not exist, `no responders` error will be returned.
+    ///
+    /// # Examples:
+    ///
+    /// ```no_run
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<(), async_nats::Error> {
+    /// let client = async_nats::connect("localhost:4222").await?;
+    /// let jetstream = async_nats::jetstream::new(client);
+    ///
+    /// let ack = jetstream.publish("events".to_string(), "data".into()).await?;
+    /// # Ok(())
+    /// # }
+    /// ```
     pub async fn publish(&self, subject: String, payload: Bytes) -> Result<PublishAck, Error> {
         let message = self.client.request(subject, payload).await?;
         let response = serde_json::from_slice(message.payload.as_ref())?;
@@ -70,7 +86,25 @@ impl Context {
         }
     }
 
-    /// Publish a message with headers
+    /// Publish a message with headers to a given subject associated with a stream and returns an acknowledgment from
+    /// the server that the message has been successfully delivered.
+    ///
+    /// If the stream does not exist, `no responders` error will be returned.
+    ///
+    /// # Examples:
+    ///
+    /// ```no_run
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<(), async_nats::Error> {
+    /// let client = async_nats::connect("localhost:4222").await?;
+    /// let jetstream = async_nats::jetstream::new(client);
+    ///
+    /// let mut headers = async_nats::HeaderMap::new();
+    /// headers.append("X-key", b"Value".as_ref().try_into()?);
+    /// let ack = jetstream.publish_with_headers("events".to_string(), headers, "data".into()).await?;
+    /// # Ok(())
+    /// # }
+    /// ```
     pub async fn publish_with_headers(
         &self,
         subject: String,
@@ -96,23 +130,28 @@ impl Context {
         }
     }
 
-    /// Send a request to the jetstream JSON API.
-    pub async fn request<T, V>(&self, subject: String, payload: &T) -> Result<Response<V>, Error>
-    where
-        T: ?Sized + Serialize,
-        V: DeserializeOwned,
-    {
-        let request = serde_json::to_vec(&payload).map(Bytes::from)?;
-
-        let message = self
-            .client
-            .request(format!("{}.{}", self.prefix, subject), request)
-            .await?;
-        let response = serde_json::from_slice(message.payload.as_ref())?;
-
-        Ok(response)
-    }
-
+    /// Create a JetStream [Stream] with given config and return a handle to it.
+    /// That handle can be used to manage and use [Consumer][crate::jetstream::consumer::Consumer].
+    ///
+    /// # Examples:
+    ///
+    /// ```no_run
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<(), async_nats::Error> {
+    /// use async_nats::jetstream::stream::Config;
+    /// use async_nats::jetstream::stream::DiscardPolicy;
+    /// let client = async_nats::connect("localhost:4222").await?;
+    /// let jetstream = async_nats::jetstream::new(client);
+    ///
+    /// let stream = jetstream.create_stream(Config {
+    ///     name: "events".to_string(),
+    ///     max_messages: 100_000,
+    ///     discard: DiscardPolicy::Old,
+    ///     ..Default::default()
+    /// }).await?;
+    /// # Ok(())
+    /// # }
+    /// ```
     pub async fn create_stream<S>(&self, stream_config: S) -> Result<Stream, Error>
     where
         Config: From<S>,
@@ -142,6 +181,21 @@ impl Context {
         }
     }
 
+    /// Checks for [Stream] existence on the server and returns handle to it.
+    /// That handle can be used to manage and use [Consumer][crate::jetstream::consumer::Consumer].
+    ///
+    /// # Examples:
+    ///
+    /// ```no_run
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<(), async_nats::Error> {
+    /// let client = async_nats::connect("localhost:4222").await?;
+    /// let jetstream = async_nats::jetstream::new(client);
+    ///
+    /// let stream = jetstream.get_stream("events").await?;
+    /// # Ok(())
+    /// # }
+    /// ```
     pub async fn get_stream<T: AsRef<str>>(&self, stream: T) -> Result<Stream, Error> {
         let stream = stream.as_ref();
         if stream.is_empty() {
@@ -168,6 +222,27 @@ impl Context {
         }
     }
 
+    /// Create a stream with the given configuration on the server if it is not present. Returns a handle to the stream  on the server.
+    ///
+    /// Note: This does not validate if the Stream on the server is compatible with the configuration passed in.
+    ///
+    /// # Examples:
+    ///
+    /// ```no_run
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<(), async_nats::Error> {
+    /// use async_nats::jetstream::stream::Config;
+    /// let client = async_nats::connect("localhost:4222").await?;
+    /// let jetstream = async_nats::jetstream::new(client);
+    ///
+    /// let stream = jetstream.get_or_create_stream(Config {
+    ///     name: "events".to_string(),
+    ///     max_messages: 10_000,
+    ///     ..Default::default()
+    /// }).await?;
+    /// # Ok(())
+    /// # }
+    /// ```
     pub async fn get_or_create_stream<S>(&self, stream_config: S) -> Result<Stream, Error>
     where
         S: Into<Config>,
@@ -192,6 +267,21 @@ impl Context {
         }
     }
 
+    /// Deletes a [Stream] with a given name.
+    ///
+    /// # Examples:
+    ///
+    /// ```no_run
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<(), async_nats::Error> {
+    /// use async_nats::jetstream::stream::Config;
+    /// let client = async_nats::connect("localhost:4222").await?;
+    /// let jetstream = async_nats::jetstream::new(client);
+    ///
+    /// let stream = jetstream.delete_stream("events").await?;
+    /// # Ok(())
+    /// # }
+    /// ```
     pub async fn delete_stream<T: AsRef<str>>(&self, stream: T) -> Result<DeleteStatus, Error> {
         let stream = stream.as_ref();
         if stream.is_empty() {
@@ -213,6 +303,28 @@ impl Context {
         }
     }
 
+    /// Updates a [Stream] with a given config. If specific field cannot be updated,
+    /// error is returned.
+    ///
+    /// # Examples:
+    ///
+    /// ```no_run
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<(), async_nats::Error> {
+    /// use async_nats::jetstream::stream::Config;
+    /// use async_nats::jetstream::stream::DiscardPolicy;
+    /// let client = async_nats::connect("localhost:4222").await?;
+    /// let jetstream = async_nats::jetstream::new(client);
+    ///
+    /// let stream = jetstream.update_stream(&Config {
+    ///     name: "events".to_string(),
+    ///     discard: DiscardPolicy::New,
+    ///     max_messages: 50_000,
+    ///     ..Default::default()
+    /// }).await?;
+    /// # Ok(())
+    /// # }
+    /// ```
     pub async fn update_stream(&self, config: &Config) -> Result<StreamInfo, Error> {
         let subject = format!("STREAM.UPDATE.{}", config.name);
         match self.request(subject, config).await? {
@@ -225,5 +337,41 @@ impl Context {
             ))),
             Response::Ok(info) => Ok(info),
         }
+    }
+
+    /// Send a request to the jetstream JSON API.
+    ///
+    /// This is a low level API used mostly internally, that should be used only in
+    /// specific cases when this crate API on [Consumer][crate::jetstream::consumer::Consumer] or [Stream] does not provide needed functionality.
+    ///
+    /// # Examples:
+    ///
+    /// ```
+    /// # use async_nats::jetstream::stream::StreamInfo;
+    /// # use async_nats::jetstream::response::Response;
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<(), async_nats::Error> {
+    /// let client = async_nats::connect("localhost:4222").await?;
+    /// let jetstream = async_nats::jetstream::new(client);
+    ///
+    /// let response: Response<StreamInfo> = jetstream
+    /// .request("STREAM.INFO.events".to_string(), &()).await?;
+    /// # Ok(())
+    /// # }
+    /// ```
+    pub async fn request<T, V>(&self, subject: String, payload: &T) -> Result<Response<V>, Error>
+    where
+        T: ?Sized + Serialize,
+        V: DeserializeOwned,
+    {
+        let request = serde_json::to_vec(&payload).map(Bytes::from)?;
+
+        let message = self
+            .client
+            .request(format!("{}.{}", self.prefix, subject), request)
+            .await?;
+        let response = serde_json::from_slice(message.payload.as_ref())?;
+
+        Ok(response)
     }
 }
