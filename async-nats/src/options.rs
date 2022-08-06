c2551a2d33aa93cb86c8ca1f54f79ff406141d5f
Tomasz Pietrek <tomasz@nats.io>
2022-08-06 18:12:22 +0200

    Add indent
    Signed-off-by: Tomasz Pietrek <tomasz@nats.io>

--- async-nats/src/options.rs
+++ async-nats/src/options.rs
@@ -423,11 +423,11 @@ impl ConnectOptions {
     /// # #[tokio::main]
     /// # async fn main() -> std::io::Result<()> {
     /// async_nats::ConnectOptions::new().event_callback(|event| async move {
-    /// match event {
-    /// async_nats::Event::Disconnect => println!("disconnected"),
-    ///     async_nats::Event::Reconnect => println!("reconnected"),
-    ///     async_nats::Event::ClientError(err) => println!("client error occured: {}", err),
-    ///     other => println!("other event happened: {}", other),
+    ///     match event {
+    ///     async_nats::Event::Disconnect => println!("disconnected"),
+    ///         async_nats::Event::Reconnect => println!("reconnected"),
+    ///         async_nats::Event::ClientError(err) => println!("client error occured: {}", err),
+    ///         other => println!("other event happened: {}", other),
     /// }
     /// }).connect("demo.nats.io").await?;
     /// # Ok(())
