38ef3335373e4e133abb717aafb3d55604e66c1d
Tomasz Pietrek <tomasz@nats.io>
2022-09-06 20:15:30 +0200

    Add docs and tests

--- async-nats/src/header.rs
+++ async-nats/src/header.rs
@@ -23,6 +23,22 @@ use serde::Serialize;
 
 pub const NATS_LAST_STREAM: &str = "nats-last-stream";
 
+/// A struct for handling NATS headers.
+/// Has a similar API to [http::header], but properly serializes and desiaralizes
+/// according to NATS requirements.
+///
+/// # Examples
+///
+/// ```
+/// # #[tokio::main]
+/// # async fn main() -> Result<(), async_nats::Error> {
+/// let client = async_nats::connect("demo.nats.io").await?;
+/// let mut headers = async_nats::HeaderMap::new();
+/// headers.insert("Key", "Value");
+/// client.publish_with_headers("subject".to_string(), headers, "payload".into()).await?;
+/// # Ok(())
+/// # }
+/// ```
 #[derive(Clone, PartialEq, Eq, Debug, Serialize, Default)]
 pub struct HeaderMap {
     inner: HashMap<HeaderName, HeaderValue>,
@@ -51,11 +67,35 @@ impl HeaderMap {
 }
 
 impl HeaderMap {
+    /// Inserts a new value to a [HeaderMap].
+    ///
+    /// # Examples
+    ///
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<(), async_nats::Error> {
+    /// let headers = async_nats::HeaderMap::new();
+    /// headers.insert("Key", "Value");
+    /// # Ok(())
+    /// # }
     pub fn insert<K: IntoHeaderName, V: IntoHeaderValue>(&mut self, name: K, value: V) {
         self.inner
             .insert(name.into_header_name(), value.into_header_value());
     }
 
+    /// Appends a new value to the list of values to a given key.
+    /// If the key did not exist, it will be inserted with provided value.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<(), async_nats::Error> {
+    /// let mut headers = async_nats::HeaderMap::new();
+    /// headers.append("Key", "Value");
+    /// headers.append("Key", "Another");
+    /// # Ok(())
+    /// # }
+    /// ```
     pub fn append<K: IntoHeaderName, V: ToString>(&mut self, name: K, value: V) {
         let key = name.into_header_name();
         let v = self.inner.get_mut(&key);
@@ -69,11 +109,24 @@ impl HeaderMap {
         }
     }
 
+    /// Gets a value for a given key. If key is not found, [Option::None] is returned.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// # #[tokio::main]
+    /// # async fn main() -> Result<(), async_nats::Error> {
+    /// let mut headers = async_nats::HeaderMap::new();
+    /// headers.append("Key", "Value");
+    /// let key = headers.get("Key").unwrap();
+    /// # Ok(())
+    /// # }
+    /// ```
     pub fn get<T: IntoHeaderName>(&self, name: T) -> Option<&HeaderValue> {
         self.inner.get(&name.into_header_name())
     }
 
-    pub fn to_bytes(&self) -> Vec<u8> {
+    pub(crate) fn to_bytes(&self) -> Vec<u8> {
         let mut buf = vec![];
         buf.extend_from_slice(b"NATS/1.0\r\n");
         for (k, vs) in &self.inner {
@@ -89,13 +142,29 @@ impl HeaderMap {
     }
 }
 
+/// A struct representing value of a given header.
+/// Can contain one or more elements.
+///
+/// # Examples
+///
+/// ```
+/// # #[tokio::main]
+/// # async fn main() -> Result<(), async_nats::header::ParseError> {
+/// use std::str::FromStr;
+/// let mut headers = async_nats::HeaderMap::new();
+/// headers.insert("Key", "Value");
+/// headers.insert("Another", async_nats::HeaderValue::from_str("AnotherValue")?);
+/// headers.insert("Third", "OneMore".parse()?);
+/// # Ok(())
+/// # }
+/// ```
 #[derive(Clone, PartialEq, Eq, Debug, Serialize, Default)]
 pub struct HeaderValue {
     value: HashSet<String>,
 }
 
 impl FromStr for HeaderValue {
-    type Err = crate::Error;
+    type Err = ParseError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut set = HeaderValue::new();
@@ -163,7 +232,7 @@ pub struct HeaderName {
     value: String,
 }
 impl FromStr for HeaderName {
-    type Err = crate::Error;
+    type Err = ParseError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         Ok(HeaderName {
@@ -178,11 +247,33 @@ impl AsRef<[u8]> for HeaderName {
     }
 }
 
+#[derive(Debug, Clone)]
+pub struct ParseError;
+
+impl std::fmt::Display for ParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "failed to parse header")
+    }
+}
+
+impl std::error::Error for ParseError {}
+
 #[cfg(test)]
 mod tests {
-    use std::{collections::HashSet, str::from_utf8};
+    use std::{
+        collections::HashSet,
+        str::{from_utf8, FromStr},
+    };
 
-    use crate::HeaderMap;
+    use crate::{HeaderMap, HeaderValue};
+
+    #[test]
+    fn try_from() -> Result<(), super::ParseError> {
+        let mut headers = HeaderMap::new();
+        headers.insert("name", HeaderValue::from_str("something")?);
+        headers.insert("name", "something2");
+        Ok(())
+    }
 
     #[test]
     fn append() {
@@ -193,6 +284,17 @@ mod tests {
         assert_eq!(
             headers.get("Key").unwrap().value,
             HashSet::from_iter(["value".to_string(), "second_value".to_string()])
+        );
+    }
+
+    #[test]
+    fn insert() {
+        let mut headers = HeaderMap::new();
+        headers.insert("Key", "Value");
+
+        assert_eq!(
+            headers.get("Key").unwrap().value,
+            HashSet::from_iter(["Value".to_string()])
         );
     }
 
