89a5759e1277c5ed59ada742207334f2f3a3398d
Tomasz Pietrek <tomasz@nats.io>
2022-09-30 11:33:47 +0200

    Use allow_direct by default in KV

--- async-nats/src/jetstream/context.rs
+++ async-nats/src/jetstream/context.rs
@@ -477,6 +477,7 @@ impl Context {
                 allow_rollup: true,
                 deny_delete: true,
                 deny_purge: false,
+                allow_direct: true,
                 num_replicas,
                 discard: stream::DiscardPolicy::New,
                 ..Default::default()
