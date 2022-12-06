fffd8be8c22678929f94c04acdf7721b2b717bec
Tomasz Pietrek <tomasz@nats.io>
2022-12-06 08:50:41 +0100

    Apply fmt
    Signed-off-by: Tomasz Pietrek <tomasz@nats.io>

--- nats/examples/async_nats_bench.rs
+++ nats/examples/async_nats_bench.rs
@@ -124,9 +124,7 @@ fn main() -> std::io::Result<()> {
         let frequency = 1000 * messages as u64 / millis;
         let mbps = (args.message_size * messages) as u64 / millis / 1024;
 
-        println!(
-            "duration: {end:?} frequency: {frequency} mbps: {mbps}"
-        );
+        println!("duration: {end:?} frequency: {frequency} mbps: {mbps}");
 
         Ok(())
     })
