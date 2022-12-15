d66b35bff3da1c6a181a92d4d73f0da0bc3f3641
ZhengSheng0524 <j13tw@yahoo.com.tw>
2022-12-15 17:09:12 +0800

    fix code lint with fmt
    Signed-off-by: ZhengSheng0524 <j13tw@yahoo.com.tw>

--- nats/src/jetstream/mod.rs
+++ nats/src/jetstream/mod.rs
@@ -1785,9 +1785,8 @@ impl JetStream {
     where
         Res: DeserializeOwned,
     {
-        let res_msg = 
-            self.connection
-                .request_timeout(subject, req, Duration::from_nanos(3_000_000_000))?;
+        let req_timeout = Duration::from_nanos(3_000_000_000);
+        let res_msg = self.connection.request_timeout(subject, req, req_timeout)?;
         let res: ApiResponse<Res> = serde_json::de::from_slice(&res_msg.data)?;
         match res {
             ApiResponse::Ok(stream_info) => Ok(stream_info),
