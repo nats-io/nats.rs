4b761069005652ad4e7f6c2d3279b5a28b3a83ce
Tomasz Pietrek <tomasz@nats.io>
2023-07-26 09:24:15 +0200

    Remove tracing from tests
    Signed-off-by: Tomasz Pietrek <tomasz@nats.io>

--- async-nats/tests/jetstream_tests.rs
+++ async-nats/tests/jetstream_tests.rs
@@ -2137,11 +2137,7 @@ mod jetstream {
     #[cfg(feature = "slow_tests")]
     #[tokio::test]
     async fn pull_consumer_stream_with_heartbeat() {
-        tracing_subscriber::fmt()
-            .with_max_level(Level::DEBUG)
-            .init();
-
-        use tracing::{debug, Level};
+        use tracing::debug;
         let server = nats_server::run_server("tests/configs/jetstream.conf");
         let client = ConnectOptions::new()
             .event_callback(|err| async move { println!("error: {err:?}") })
