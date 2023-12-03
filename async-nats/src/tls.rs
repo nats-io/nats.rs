d5d9993af47cfc98e2f3770387d08b87ea63d718
Paolo Barbolini <paolo.barbolini@m4ss.net>
2023-12-03 10:56:40 +0100

    Update rustls to v0.22 and related dependencies

--- async-nats/src/tls.rs
+++ async-nats/src/tls.rs
@@ -13,72 +13,48 @@
 
 use crate::connector::ConnectorOptions;
 use crate::tls;
-use std::fs::File;
 use std::io::{self, BufReader, ErrorKind};
 use std::path::PathBuf;
-use tokio_rustls::rustls::{self, Certificate, OwnedTrustAnchor, PrivateKey};
-use webpki::TrustAnchor;
+use tokio_rustls::rustls::{ClientConfig, RootCertStore};
+use webpki::types::{CertificateDer, PrivateKeyDer};
 
 /// Loads client certificates from a `.pem` file.
 /// If the pem file is found, but does not contain any certificates, it will return
 /// empty set of Certificates, not error.
 /// Can be used to parse only client certificates from .pem file containing both client key and certs.
-pub(crate) async fn load_certs(path: PathBuf) -> io::Result<Vec<Certificate>> {
+pub(crate) async fn load_certs(path: PathBuf) -> io::Result<Vec<CertificateDer<'static>>> {
     tokio::task::spawn_blocking(move || {
         let file = std::fs::File::open(path)?;
         let mut reader = BufReader::new(file);
-        let certs = rustls_pemfile::certs(&mut reader)?
-            .into_iter()
-            .map(Certificate)
-            .collect();
-        Ok(certs)
+        rustls_pemfile::certs(&mut reader).collect::<io::Result<Vec<_>>>()
     })
     .await?
 }
 
 /// Loads client key from a `.pem` file.
 /// Can be used to parse only client key from .pem file containing both client key and certs.
-pub(crate) async fn load_key(path: PathBuf) -> io::Result<PrivateKey> {
+pub(crate) async fn load_key(path: PathBuf) -> io::Result<PrivateKeyDer<'static>> {
     tokio::task::spawn_blocking(move || {
         let file = std::fs::File::open(path)?;
         let mut reader = BufReader::new(file);
-
-        loop {
-            match rustls_pemfile::read_one(&mut reader)? {
-                Some(rustls_pemfile::Item::RSAKey(key))
-                | Some(rustls_pemfile::Item::PKCS8Key(key))
-                | Some(rustls_pemfile::Item::ECKey(key)) => return Ok(PrivateKey(key)),
-                // if public key is found, don't error, just skip it and hope to find client key next.
-                Some(rustls_pemfile::Item::X509Certificate(_)) | Some(_) => {}
-                None => break,
-            }
-        }
-
-        Err(io::Error::new(
-            ErrorKind::NotFound,
-            "could not find client key in the path",
-        ))
+        rustls_pemfile::private_key(&mut reader)?.ok_or_else(|| {
+            io::Error::new(ErrorKind::NotFound, "could not find client key in the path")
+        })
     })
     .await?
 }
 
-pub(crate) async fn config_tls(options: &ConnectorOptions) -> io::Result<rustls::ClientConfig> {
-    let mut root_store = tokio_rustls::rustls::RootCertStore::empty();
+pub(crate) async fn config_tls(options: &ConnectorOptions) -> io::Result<ClientConfig> {
+    let mut root_store = RootCertStore::empty();
     // load native system certs only if user did not specify them.
     if options.tls_client_config.is_some() || options.certificates.is_empty() {
-        root_store.add_parsable_certificates(
-            rustls_native_certs::load_native_certs()
-                .map_err(|err| {
-                    io::Error::new(
-                        ErrorKind::Other,
-                        format!("could not load platform certs: {err}"),
-                    )
-                })?
-                .into_iter()
-                .map(|cert| cert.0)
-                .collect::<Vec<Vec<u8>>>()
-                .as_slice(),
-        );
+        let certs_iter = rustls_native_certs::load_native_certs().map_err(|err| {
+            io::Error::new(
+                ErrorKind::Other,
+                format!("could not load platform certs: {err}"),
+            )
+        })?;
+        root_store.add_parsable_certificates(certs_iter);
     }
 
     // use provided ClientConfig or built it from options.
@@ -88,28 +64,20 @@ pub(crate) async fn config_tls(options: &ConnectorOptions) -> io::Result<rustls:
         } else {
             // Include user-provided certificates.
             for cafile in &options.certificates {
-                let mut pem = BufReader::new(File::open(cafile)?);
-                let certs = rustls_pemfile::certs(&mut pem)?;
-                let trust_anchors = certs.iter().map(|cert| {
-                    let ta = TrustAnchor::try_from_cert_der(&cert[..])
-                        .map_err(|err| {
-                            io::Error::new(
-                                ErrorKind::InvalidInput,
-                                format!("could not load certs: {err}"),
-                            )
-                        })
-                        .unwrap();
-                    OwnedTrustAnchor::from_subject_spki_name_constraints(
-                        ta.subject,
-                        ta.spki,
-                        ta.name_constraints,
-                    )
-                });
-                root_store.add_trust_anchors(trust_anchors);
+                let trust_anchors = load_certs(cafile.to_owned())
+                    .await?
+                    .into_iter()
+                    .map(|cert| webpki::anchor_from_trusted_cert(&cert).map(|ta| ta.to_owned()))
+                    .collect::<Result<Vec<_>, webpki::Error>>()
+                    .map_err(|err| {
+                        io::Error::new(
+                            ErrorKind::InvalidInput,
+                            format!("could not load certs: {err}"),
+                        )
+                    })?;
+                root_store.extend(trust_anchors);
             }
-            let builder = rustls::ClientConfig::builder()
-                .with_safe_defaults()
-                .with_root_certificates(root_store);
+            let builder = ClientConfig::builder().with_root_certificates(root_store);
             if let Some(cert) = options.client_cert.clone() {
                 if let Some(key) = options.client_key.clone() {
                     let key = tls::load_key(key).await?;
