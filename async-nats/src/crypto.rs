2735e317de9b7885a7f4ea7790756bb9183b6d52
Paolo Barbolini <paolo.barbolini@m4ss.net>
2024-03-11 18:31:01 +0100

    Use the same crypto backend as rustls on our side too

--- async-nats/src/crypto.rs
+++ async-nats/src/crypto.rs
@@ -1,4 +1,12 @@
-use ring::digest::{Context, SHA256};
+#[cfg(feature = "aws-lc-rs")]
+use aws_lc_rs as crypto_backend;
+#[cfg(not(feature = "aws-lc-rs"))]
+use ring as crypto_backend;
+
+#[cfg(not(any(feature = "aws-lc-rs", feature = "ring")))]
+compile_error!("Please enable the `aws-lc-rs` or `ring` feature");
+
+use crypto_backend::digest::{Context, SHA256};
 
 pub(crate) struct Sha256(Context);
 
