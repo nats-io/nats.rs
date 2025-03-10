fa7d7576095d24c0dde872cad5b246bb359d3307
Tomasz Pietrek <tomasz@nats.io>
2025-03-10 11:36:38 +0100

    Some debugging
    Signed-off-by: Tomasz Pietrek <tomasz@nats.io>

--- async-nats/tests/jetstream_tests.rs
+++ async-nats/tests/jetstream_tests.rs
@@ -2220,6 +2220,7 @@ mod jetstream {
             .unwrap();
 
         let stream = context.get_stream("events").await.unwrap();
+        println!("STREAM INFO: {:?}", stream.cached_info());
         stream
             .create_consumer(consumer::pull::Config {
                 durable_name: Some("pull".into()),
@@ -2228,6 +2229,7 @@ mod jetstream {
             .await
             .unwrap();
         let consumer: PullConsumer = stream.get_consumer("pull").await.unwrap();
+        println!("CONSUMER INFO: {:?}", consumer.cached_info());
 
         // Delete the consumer before starting fetching messages.
         let name = &consumer.cached_info().name;
