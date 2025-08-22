9f6148fbcc9023903ec2ebc9730b583f38a52eb4
Tomasz Pietrek <tomasz@nats.io>
2025-08-22 12:37:20 +0200

    Add basic docs example
    Signed-off-by: Tomasz Pietrek <tomasz@nats.io>

---
+++ async-nats/examples/docs_basics_publish.rs
@@ -0,0 +1,11 @@
+#[tokio::main]
+async fn main() {
+    let nc = async_nats::connect("nats://localhost:4222").await.unwrap();
+
+    // NATS-DOC-START
+    // Publish a message to the "greetings" subject
+    nc.publish("weather.updates", "Temperature: 72°F".into())
+        .await
+        .unwrap();
+    // NATS-DOC-END
+}
