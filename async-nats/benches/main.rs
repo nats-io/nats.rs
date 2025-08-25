87074396850b667ab74889d371d981b5bc7952f4
Tomasz Pietrek <tomasz@nats.io>
2025-08-25 12:14:55 +0200

    Add subject validataion
    Signed-off-by: Tomasz Pietrek <tomasz@nats.io>
    Signed-off-by: Tomasz Pietrek <tomasz@synadia.com>

--- async-nats/benches/main.rs
+++ async-nats/benches/main.rs
@@ -3,5 +3,10 @@ use criterion::criterion_main;
 // Import the benchmark groups from both files
 mod core_nats;
 mod jetstream;
+mod subject_validation;
 
-criterion_main!(core_nats::core_nats, jetstream::jetstream);
+criterion_main!(
+    core_nats::core_nats,
+    jetstream::jetstream,
+    subject_validation::subject_validation
+);
