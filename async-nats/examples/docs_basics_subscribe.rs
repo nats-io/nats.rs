5ecd21a32fe2f8f6e8a0fab3aa928d39fa659ae5
scottf <scottfauerbach@gmail.com>
2026-04-29 07:59:52 -0400

    Fix copy-paste comment for subscribe example

--- async-nats/examples/docs_basics_subscribe.rs
+++ async-nats/examples/docs_basics_subscribe.rs
@@ -3,7 +3,7 @@ async fn main() {
     let nc = async_nats::connect("nats://localhost:4222").await.unwrap();
 
     // NATS-DOC-START
-    // Publish a message to the "greetings" subject
+    // Subscribe to the subject weather updates
     nc.subscribe("weather.updates").await.unwrap();
     // NATS-DOC-END
 }
